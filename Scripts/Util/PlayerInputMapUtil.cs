007de6c3ac1e62b0df03ca908c382f30f92caf7f
= <franciscuevas4@gmail.com>
2024-01-14 02:06:31 -0800

    Add free_look input. Make PlayerInputMapUtil all readonly

--- Scripts/Util/PlayerInputMapUtil.cs
+++ Scripts/Util/PlayerInputMapUtil.cs
@@ -4,11 +4,12 @@ namespace FPSController.Scripts.Util;
 
 public static class PlayerInputMapUtil
 {
-    public static StringName MoveForward = "move_forward";
-    public static StringName MoveBackward = "move_backward";
-    public static StringName MoveLeft = "move_left";
-    public static StringName MoveRight = "move_right";
-    public static StringName MoveJump = "move_jump";
-    public static StringName MoveCrouch = "move_crouch";
-    public static StringName MoveSprint = "move_sprint";
+    public static readonly StringName MoveForward = "move_forward";
+    public static readonly StringName MoveBackward = "move_backward";
+    public static readonly StringName MoveLeft = "move_left";
+    public static readonly StringName MoveRight = "move_right";
+    public static readonly StringName MoveJump = "move_jump";
+    public static readonly StringName MoveCrouch = "move_crouch";
+    public static readonly StringName MoveSprint = "move_sprint";
+    public static readonly StringName FreeLook = "free_look";
 }
