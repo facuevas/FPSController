930f58ac39f9a14381c08a7f4061192ee0293766
= <franciscuevas4@gmail.com>
2024-01-14 02:42:06 -0800

    Init free looking

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -20,10 +20,13 @@ public partial class Player : CharacterBody3D
     private const float SprintingSpeed = 8.0f;
     private const float CrouchingSpeed = 3.0f;
     private const float LerpSpeed = 10.0f;
-    private const float StandingHeight = 1.8f;
-    private const float CrouchingHeight = 1.3f;
-    private bool _isCrouching;
+
+    // Player state
+    private bool _isWalking;
     private bool _isSprinting;
+    private bool _isCrouching;
+    private bool _isFreeLooking;
+    private bool _isSliding;
 
     // Mouse Sensitivity
     // We will refactor this out eventually
@@ -31,6 +34,7 @@ public partial class Player : CharacterBody3D
     public float MouseSensitivity { get; set; } = 0.1f;
 
     // Node references
+    private Node3D _neck;
     private Node3D _head;
     private CollisionShape3D _standingCollisionShape;
     private CollisionShape3D _crouchingCollisionShape;
@@ -39,7 +43,8 @@ public partial class Player : CharacterBody3D
     public override void _Ready()
     {
         Input.MouseMode = Input.MouseModeEnum.Captured;
-        _head = GetNode<Node3D>("Head");
+        _neck = GetNode<Node3D>("Neck");
+        _head = GetNode<Node3D>("Neck/Head");
         _standingCollisionShape = GetNode<CollisionShape3D>("StandingCollisionShape");
         _crouchingCollisionShape = GetNode<CollisionShape3D>("CrouchingCollisionShape");
         _rayCast = GetNode<RayCast3D>("RayCast3D");
@@ -56,10 +61,27 @@ public partial class Player : CharacterBody3D
         {
             // Mouse Movement
             var mouseMotion = (InputEventMouseMotion)@event;
-            RotateY(Mathf.DegToRad(-mouseMotion.Relative.X * MouseSensitivity));
-            _head.RotateX(Mathf.DegToRad(-mouseMotion.Relative.Y * MouseSensitivity));
-            _head.Rotation = new Vector3(Mathf.Clamp(_head.Rotation.X, Mathf.DegToRad(-89f), Mathf.DegToRad(89f)), 0f,
-                0f);
+
+            // Free look
+            if (_isFreeLooking)
+            {
+                _neck.RotateY(Mathf.DegToRad(-mouseMotion.Relative.X * MouseSensitivity));
+                _neck.Rotation = new Vector3(
+                    0f,
+                    Mathf.Clamp(_neck.Rotation.Y, Mathf.DegToRad(-120f), Mathf.DegToRad(120f)),
+                    0f
+                );
+            }
+            else
+            {
+                // Non-free look
+                RotateY(Mathf.DegToRad(-mouseMotion.Relative.X * MouseSensitivity));
+                _head.RotateX(Mathf.DegToRad(-mouseMotion.Relative.Y * MouseSensitivity));
+                _head.Rotation = new Vector3(Mathf.Clamp(_head.Rotation.X, Mathf.DegToRad(-89f), Mathf.DegToRad(89f)),
+                    0f,
+                    0f
+                );
+            }
         }
     }
 
@@ -69,6 +91,7 @@ public partial class Player : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
+        PlayerFreeLook(delta);
         PlayerCrouch(delta);
         var velocity = PlayerMove(delta);
         Velocity = velocity;
@@ -123,21 +146,35 @@ public partial class Player : CharacterBody3D
         // Player Crouch or Stand logic
         if (Input.IsActionPressed(PlayerInputMapUtil.MoveCrouch))
         {
-            _head.Position = _head.Position.Lerp(new Vector3(0f, CrouchingHeight, 0f), LerpSpeed * (float)delta);
+            _head.Position = _head.Position.Lerp(new Vector3(0f, -0.5f, 0f), LerpSpeed * (float)delta);
             _standingCollisionShape.Disabled = true;
             _crouchingCollisionShape.Disabled = false;
-            _isCrouching = true;
+            _isWalking = false;
             _isSprinting = false;
+            _isCrouching = true;
             CurrentSpeed = GetPlayerSpeed();
         }
         else if (!_rayCast.IsColliding())
         {
-            _head.Position = _head.Position.Lerp(new Vector3(0f, StandingHeight, 0f), LerpSpeed * (float)delta);
+            _head.Position = _head.Position.Lerp(new Vector3(0f, 0f, 0f), LerpSpeed * (float)delta);
             _standingCollisionShape.Disabled = false;
             _crouchingCollisionShape.Disabled = true;
             _isCrouching = false;
             _isSprinting = Input.IsActionPressed(PlayerInputMapUtil.MoveSprint);
+            _isWalking = !_isSprinting;
             CurrentSpeed = GetPlayerSpeed();
+        }
+    }
+
+    private void PlayerFreeLook(double delta)
+    {
+        _isFreeLooking = Input.IsActionPressed(PlayerInputMapUtil.FreeLook);
+        if (!_isFreeLooking)
+        {
+            // Reset the neck if we are no longer free looking.
+            //_neck.Rotation.Lerp(Vector3.Zero, (float)delta * LerpSpeed);
+            var neckLerp = Mathf.Lerp(_neck.Rotation.Y, 0.0f, LerpSpeed * (float)delta);
+            _neck.Rotation = new Vector3(0f, neckLerp, 0f);
         }
     }
 
@@ -157,7 +194,7 @@ public partial class Player : CharacterBody3D
         {
             return SprintingSpeed;
         }
-        
+
         return WalkingSpeed;
     }
 }
